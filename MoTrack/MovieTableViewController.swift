e61a7caacdec9804464106cadc7899508b04d234
arinjay <arinjaysharma421@gmail.com>
2017-01-04 21:17:08 +0530

    displaying structured data

--- MoTrack/MovieTableViewController.swift
+++ MoTrack/MovieTableViewController.swift
@@ -10,24 +10,27 @@ import UIKit
 
 class MovieTableViewController: UITableViewController {
 
+    var nowPlaying = [Movie]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        Movie.nowPlaying { (sucess:Bool,movieList:[Movie]?) in
-            
-            for movie in movieList! {
-                    print(movie.title)
-            }
+        loadMovie()
         }
         
         
+        func loadMovie(){
+            Movie.nowPlaying { (sucess:Bool,movieList:[Movie]?) in
+                if sucess {
+                    self.nowPlaying = movieList!
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()  // so that update is performed on main thread
+                    }
+                }
+        }
+        
         
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,24 +41,23 @@ class MovieTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return nowPlaying.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let movie = nowPlaying[indexPath.row]
+        cell.textLabel?.text = movie.title
+        cell.detailTextLabel?.text = movie.description
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
